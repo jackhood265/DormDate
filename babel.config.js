55b24fdb01d71b0cae925fe95b07018ee9336575
Jack Hood <jacksterjackman@icloud.com>
2025-10-21 13:52:29 -0500

    iOS: remove reanimated/worklets to fix RCTTurboModule header error; clean prebuild

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      // 👇 must always be the LAST plugin
-      "react-native-reanimated/plugin",
-    ],
   };
 };
