6a70cdc31f8abdd0731cba022bb851f9ed981a7f
Jack Hood <jacksterjackman@icloud.com>
2025-11-25 16:53:32 -0600

    clear sentinel.entry and update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-﻿import 'react-native-gesture-handler';  // must be first
-import './sentinel.entry';
+﻿import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 import App from './App';
+
 registerRootComponent(App);
