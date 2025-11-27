ef5d6b80f0397f6d2f68e421b98a4e795d68dfff
Jack Hood <jacksterjackman@icloud.com>
2025-11-27 17:10:28 -0600

    fix onboarding greek crash

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -1,6 +1,4 @@
-// navigation/AppNavigator.tsx
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import LoginScreen from "../screens/LoginScreen";
@@ -20,29 +18,23 @@ const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      {/* Auth */}
+      <Stack.Screen name="Login" component={LoginScreen} />
 
-        {/* Auth */}
-        <Stack.Screen name="Login" component={LoginScreen} />
+      {/* Onboarding */}
+      <Stack.Screen name="OnboardingName" component={OnboardingNameScreen} />
+      <Stack.Screen name="OnboardingGender" component={OnboardingGenderScreen} />
+      <Stack.Screen name="OnboardingClassYear" component={OnboardingClassYearScreen} />
+      <Stack.Screen name="OnboardingGreek" component={OnboardingGreekScreen} />
+      <Stack.Screen name="OnboardingLocation" component={OnboardingLocationScreen} />
+      <Stack.Screen name="OnboardingBio" component={OnboardingBioScreen} />
+      <Stack.Screen name="OnboardingPhoto" component={OnboardingPhotoScreen} />
 
-        {/* Onboarding */}
-        <Stack.Screen name="OnboardingName" component={OnboardingNameScreen} />
-        <Stack.Screen name="OnboardingGender" component={OnboardingGenderScreen} />
-        <Stack.Screen name="OnboardingClassYear" component={OnboardingClassYearScreen} />
-        <Stack.Screen name="OnboardingGreek" component={OnboardingGreekScreen} />
-        <Stack.Screen name="OnboardingLocation" component={OnboardingLocationScreen} />
-        <Stack.Screen name="OnboardingBio" component={OnboardingBioScreen} />
-        <Stack.Screen name="OnboardingPhoto" component={OnboardingPhotoScreen} />
-
-        {/* Main App */}
-        <Stack.Screen name="MainTabs" component={BottomTabs} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
-        <Stack.Screen name="EditProfile" component={EditProfileScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+      {/* Main App */}
+      <Stack.Screen name="MainTabs" component={BottomTabs} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+      <Stack.Screen name="EditProfile" component={EditProfileScreen} />
+    </Stack.Navigator>
   );
 }
-
-
